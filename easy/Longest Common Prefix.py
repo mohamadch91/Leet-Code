c6defdc9abe422f320f775212c5019f977e589ed
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-09-26 22:39:44 +0330

    first solution

---
+++ easy/Longest Common Prefix.py
@@ -0,0 +1,18 @@
+strs = ["ab", "a"]
+common =""
+for i in range(len(strs[0])):
+    flag = False
+    for j in range(1,len(strs)):
+        if(i>=len(strs[j])):
+            flag = True
+            break
+        if strs[0][i]==strs[j][i]:
+            continue
+        else:
+            flag = True
+            break
+    if flag:
+        break
+    common+=strs[0][i]
+
+print(common)
